24266bb2a94df4fe091f3fa5d450e22a5845380b
chenchang <chench@kids-live.cn>
2017-07-10 09:10:36 +0800

    new start

---
+++ service/business/src/main/java/cn/fintecher/PangolinBusinessApplication.java
@@ -0,0 +1,12 @@
+package cn.fintecher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PangolinBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PangolinBusinessApplication.class, args);
+	}
+}
