b0764e8316a0e1f081ce3115380496885ee8f85d
gaobeibei <572192995@qq.com>
2017-07-28 13:59:06 +0800

    APP客户地址，联系人查找（按数据来源倒序）

--- service/business/src/main/java/cn/fintecher/pangolin/business/webapp/PersonalAppController.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/webapp/PersonalAppController.java
@@ -1,20 +1,31 @@
 package cn.fintecher.pangolin.business.webapp;
 
+import cn.fintecher.pangolin.business.model.AddressRepairInfo;
+import cn.fintecher.pangolin.business.model.RepairInfoModel;
+import cn.fintecher.pangolin.business.repository.PersonalAddressRepository;
+import cn.fintecher.pangolin.business.repository.PersonalContactRepository;
 import cn.fintecher.pangolin.business.repository.PersonalRepository;
 import cn.fintecher.pangolin.business.web.BaseController;
-import cn.fintecher.pangolin.entity.Personal;
+import cn.fintecher.pangolin.entity.*;
+import cn.fintecher.pangolin.util.ZWDateUtil;
+import cn.fintecher.pangolin.web.HeaderUtil;
 import cn.fintecher.pangolin.web.ResponseUtil;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.util.FileUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.collections4.IterableUtils;
+import org.apache.commons.collections4.IteratorUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.inject.Inject;
+import java.net.URISyntaxException;
+import java.util.List;
 
 /**
  * @author : gaobeibei
@@ -32,12 +43,37 @@ public class PersonalAppController extends BaseController {
     @Inject
     PersonalRepository personalRepository;
 
+    @Inject
+    PersonalAddressRepository personalAddressRepository;
+
+    @Inject
+    PersonalContactRepository personalContactRepository;
 
     @GetMapping("/getPersonalForApp")
-    @ApiOperation(value = "根据客户ID获取客户信息", notes = "根据客户ID获取客户信息")
+    @ApiOperation(value = "查询客户信息for APP", notes = "查询客户信息for APP")
     public ResponseEntity<Personal> getPersonal(@RequestParam @ApiParam(value = "客户ID", required = true) String id) {
         log.debug("REST request to get personal : {}", id);
         Personal personal = personalRepository.findOne(id);
-        return ResponseEntity.ok().body(personal);
+        return ResponseEntity.ok().headers(HeaderUtil.createAlert("查询成功", "Personal")).body(personal);
+    }
+
+    @GetMapping("/getContactInfoForApp")
+    @ResponseBody
+    @ApiOperation(value = "查询联系人信息for APP", notes = "查询联系人信息for APP")
+    public ResponseEntity<List<PersonalContact>> getContactInfoForApp(@RequestParam @ApiParam(value = "客户ID", required = true) String id) {
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(QPersonalContact.personalContact.personalId.eq(id));
+        List<PersonalContact> personalContactList = IterableUtils.toList(personalContactRepository.findAll(builder, new Sort(Sort.Direction.DESC, "source")));
+        return ResponseEntity.ok().headers(HeaderUtil.createAlert("查询成功", "PersonalContactList")).body(personalContactList);
+    }
+
+    @GetMapping("/getAddressInfoForApp")
+    @ResponseBody
+    @ApiOperation(value = "查询地址信息for APP", notes = "查询地址信息for APP")
+    public ResponseEntity<List<PersonalAddress>> getAddressInfoForApp(@RequestParam @ApiParam(value = "客户ID", required = true) String id) {
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(QPersonalAddress.personalAddress.personalId.eq(id));
+        List<PersonalAddress> personalAddressList = IterableUtils.toList(personalAddressRepository.findAll(builder, new Sort(Sort.Direction.DESC, "source")));
+        return ResponseEntity.ok().headers(HeaderUtil.createAlert("查询成功", "PersonalAddressList")).body(personalAddressList);
     }
 }
