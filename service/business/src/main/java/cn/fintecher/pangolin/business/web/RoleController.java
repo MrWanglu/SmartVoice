f1a718e4e89eae71e2409f1505a403f5fc05b013
lvguorong <lvguorong>
2017-07-28 10:39:00 +0800

    提交RoleController

--- service/business/src/main/java/cn/fintecher/pangolin/business/web/RoleController.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/web/RoleController.java
@@ -115,8 +115,8 @@ public class RoleController extends BaseController {
         //更新角色的公司code码需要前端传
         //判断角色的名称是否重复
         QRole qRole = QRole.role;
-        Iterator<Role> roles = roleRepository.findAll(qRole.id.ne(role.getId()).and(qRole.name.eq(role.getName())).and(qRole.companyCode.eq(user.getCompanyCode()))).iterator();
-        if (roles.hasNext()){
+        boolean exists = roleRepository.exists(qRole.id.ne(role.getId()).and(qRole.name.eq(role.getName())).and(qRole.companyCode.eq(user.getCompanyCode())));
+        if (exists){
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "The role name has been occupied", "该角色名已被占用")).body(null);
         }else {
             //判断角色的状态
@@ -146,15 +146,20 @@ public class RoleController extends BaseController {
     @GetMapping("/getRole")
     @ApiOperation(value = "查找角色通过id", notes = "查找角色通过id")
     public ResponseEntity<Role> getRole(@ApiParam(value = "角色id", required = true) @RequestParam(value = "id") String id) throws URISyntaxException {
+        try {
         Role role = roleRepository.findOne(id);
         return ResponseEntity.ok().body(role);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("获取失败", ENTITY_NAME, e.getMessage())).body(null);
+        }
     }
 
     @GetMapping("/getRoleRes")
     @ApiOperation(value = "角色查找资源", notes = "角色查找资源")
     public ResponseEntity<List<Resource>> getRoleRes(@ApiParam(value = "角色id", required = true) @RequestParam(value = "id") String id,
                                                      @QuerydslPredicate(root = Role.class) Predicate predicate) throws URISyntaxException {
-
+        try {
         QResource qResource = QResource.resource;
         Iterator<Resource> resources = resourceRepository.findAll(qResource.roles.any().id.eq(id)).iterator();
         List<Resource> resourceList = new ArrayList<Resource>();
@@ -162,6 +167,10 @@ public class RoleController extends BaseController {
             resourceList.add(resources.next());
         }
         return ResponseEntity.ok().body(resourceList);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createEntityCreationAlert("未查找到资源", ENTITY_NAME)).body(null);
+        }
     }
 
 
@@ -224,7 +233,12 @@ public class RoleController extends BaseController {
     @ApiOperation(value = "查询所有角色分页", notes = "查询所有角色分页")
     public ResponseEntity<Page<Role>> getAllRole(@ApiIgnore Pageable pageable) throws URISyntaxException {
         logger.debug("REST request to get all of Role");
+        try {
         Page<Role> page = roleRepository.findAll(pageable);
         return new ResponseEntity<>(page, HttpStatus.OK);
+        } catch (Exception e) {
+        logger.error(e.getMessage(), e);
+        return ResponseEntity.badRequest().headers(HeaderUtil.createEntityCreationAlert("查找失败", ENTITY_NAME)).body(null);
+    }
     }
 }
