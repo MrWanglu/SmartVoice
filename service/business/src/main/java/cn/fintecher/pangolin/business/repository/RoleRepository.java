50d098a4ed6f490ab12c59110e45336fb66e6db1
hukaijia <hukaijia>
2017-07-29 10:45:45 +0800

    角色代码修改优化

--- service/business/src/main/java/cn/fintecher/pangolin/business/repository/RoleRepository.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/repository/RoleRepository.java
@@ -4,9 +4,13 @@ import cn.fintecher.pangolin.entity.QRole;
 import cn.fintecher.pangolin.entity.Role;
 import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @Author: PeiShouWen
@@ -21,4 +25,7 @@ public interface RoleRepository extends QueryDslPredicateExecutor<Role>, JpaRepo
         bindings.bind(root.name).first((path, value) -> path.like(value));
         bindings.bind(root.status).first((path, value) -> path.eq(value));
     }
+
+    @Query(value = "delete from acc_roleres where role_id=:roleId", nativeQuery = true)
+    int deleteResoByRoleId(@Param("roleId") String roleId);
 }
--- service/business/src/main/java/cn/fintecher/pangolin/business/web/RoleController.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/web/RoleController.java
@@ -11,6 +11,7 @@ import cn.fintecher.pangolin.web.HeaderUtil;
 import cn.fintecher.pangolin.web.PaginationUtil;
 import com.querydsl.core.types.Predicate;
 import io.swagger.annotations.*;
+import org.apache.commons.collections4.IteratorUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,6 @@ import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
@@ -86,10 +86,10 @@ public class RoleController extends BaseController {
         }
         //增加角色的code需要传入
         QRole qRole = QRole.role;
-        Iterator<Role> roles = roleRepository.findAll(qRole.name.eq(role.getName()).and(qRole.companyCode.eq(user.getCompanyCode()))).iterator();
-        if (roles.hasNext()){
+        boolean exist = roleRepository.exists(qRole.name.eq(role.getName()).and(qRole.companyCode.eq(user.getCompanyCode())));
+        if (exist) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "The role name has been occupied", "该角色名已被占用")).body(null);
-        }else {
+        } else {
             role.setOperator(user.getUserName());
             role.setOperateTime(ZWDateUtil.getNowDateTime());
             Role role1 = roleRepository.save(role);
@@ -116,22 +116,17 @@ public class RoleController extends BaseController {
         //判断角色的名称是否重复
         QRole qRole = QRole.role;
         boolean exists = roleRepository.exists(qRole.id.ne(role.getId()).and(qRole.name.eq(role.getName())).and(qRole.companyCode.eq(user.getCompanyCode())));
-        if (exists){
+        if (exists) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "The role name has been occupied", "该角色名已被占用")).body(null);
-        }else {
+        } else {
             //判断角色的状态
             if (Objects.equals(Status.Disable.getValue(), role.getStatus())) {
                 QUser qUser = QUser.user;
-                Iterable<User> users = userRepository.findAll(qUser.roles.any().id.eq(role.getId()));
-                if (users.iterator().hasNext()){
+                if (userRepository.exists(qUser.roles.any().id.eq(role.getId()))) {
                     return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "The role of related user, please delete the user", "该角色下有关联的用户，请先删除用户")).body(null);
-                }else {
-                    Role role1 = roleRepository.findOne(role.getId());
-                    Iterable<Resource> resources = resourceRepository.findAll(QResource.resource.roles.contains(role1));
-                    resources.forEach(e->{
-                        e.getRoles().remove(role1);
-                        resourceRepository.saveAndFlush(e);
-                    });
+                } else {
+                    //解除角色与资源的关系
+                    roleRepository.deleteResoByRoleId(role.getId());
                     Role roleNew = roleRepository.save(role);
                     return ResponseEntity.ok().body(roleNew);
                 }
@@ -147,8 +142,8 @@ public class RoleController extends BaseController {
     @ApiOperation(value = "查找角色通过id", notes = "查找角色通过id")
     public ResponseEntity<Role> getRole(@ApiParam(value = "角色id", required = true) @RequestParam(value = "id") String id) throws URISyntaxException {
         try {
-        Role role = roleRepository.findOne(id);
-        return ResponseEntity.ok().body(role);
+            Role role = roleRepository.findOne(id);
+            return ResponseEntity.ok().body(role);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("获取失败", ENTITY_NAME, e.getMessage())).body(null);
@@ -160,13 +155,10 @@ public class RoleController extends BaseController {
     public ResponseEntity<List<Resource>> getRoleRes(@ApiParam(value = "角色id", required = true) @RequestParam(value = "id") String id,
                                                      @QuerydslPredicate(root = Role.class) Predicate predicate) throws URISyntaxException {
         try {
-        QResource qResource = QResource.resource;
-        Iterator<Resource> resources = resourceRepository.findAll(qResource.roles.any().id.eq(id)).iterator();
-        List<Resource> resourceList = new ArrayList<Resource>();
-        while (resources.hasNext()) {
-            resourceList.add(resources.next());
-        }
-        return ResponseEntity.ok().body(resourceList);
+            QResource qResource = QResource.resource;
+            Iterator<Resource> resources = resourceRepository.findAll(qResource.roles.any().id.eq(id)).iterator();
+            List<Resource> resourceList = IteratorUtils.toList(resources);
+            return ResponseEntity.ok().body(resourceList);
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
             return ResponseEntity.badRequest().headers(HeaderUtil.createEntityCreationAlert("未查找到资源", ENTITY_NAME)).body(null);
@@ -215,16 +207,14 @@ public class RoleController extends BaseController {
         }
         Role role = roleRepository.findOne(id);
         QUser qUser = QUser.user;
-        Iterable<User> userIterator = userRepository.findAll(qUser.roles.any().id.eq(id).and(qUser.companyCode.eq(role.getCompanyCode())));
-        if (userIterator.iterator().hasNext()) {
+        boolean exist = userRepository.exists(qUser.roles.any().id.eq(id).and(qUser.companyCode.eq(role.getCompanyCode())));
+        if (exist) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME,
                     "The role has an associated user, please delete the relationship with the user first", "该角色下有关联用户，请先删除与用户的关系")).body(null);
         }
-        Iterable<Resource> resources = resourceRepository.findAll(QResource.resource.roles.contains(role));
-        resources.forEach(e->{
-            e.getRoles().remove(role);
-            resourceRepository.saveAndFlush(e);
-        });
+        //解除角色与资源的关系
+        roleRepository.deleteResoByRoleId(role.getId());
+        Role roleNew = roleRepository.save(role);
         roleRepository.delete(id);
         return ResponseEntity.ok().body(null);
     }
@@ -234,11 +224,11 @@ public class RoleController extends BaseController {
     public ResponseEntity<Page<Role>> getAllRole(@ApiIgnore Pageable pageable) throws URISyntaxException {
         logger.debug("REST request to get all of Role");
         try {
-        Page<Role> page = roleRepository.findAll(pageable);
-        return new ResponseEntity<>(page, HttpStatus.OK);
+            Page<Role> page = roleRepository.findAll(pageable);
+            return new ResponseEntity<>(page, HttpStatus.OK);
         } catch (Exception e) {
-        logger.error(e.getMessage(), e);
-        return ResponseEntity.badRequest().headers(HeaderUtil.createEntityCreationAlert("查找失败", ENTITY_NAME)).body(null);
-    }
+            logger.error(e.getMessage(), e);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createEntityCreationAlert("查找失败", ENTITY_NAME)).body(null);
+        }
     }
 }
