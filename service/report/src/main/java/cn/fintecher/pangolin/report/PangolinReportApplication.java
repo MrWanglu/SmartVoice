29eef21f3b8184ccb5966d7d063325bf3ae2bc63
xiaqun <qun.xia@zzlgroup.com>
2017-08-01 09:28:59 +0800

    新增report服务

---
+++ service/report/src/main/java/cn/fintecher/pangolin/report/PangolinReportApplication.java
@@ -0,0 +1,55 @@
+package cn.fintecher.pangolin.report;
+
+import cn.fintecher.pangolin.entity.util.Constants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author : xiaqun
+ * @Description :
+ * @Date : 20:06 2017/7/31
+ */
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+@EntityScan("cn.fintecher.pangolin.entity")
+public class PangolinReportApplication {
+    private static final Logger log = LoggerFactory.getLogger(PangolinReportApplication.class);
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+    @Bean
+    public Queue unReduceSuccessQueue() {
+        return new Queue(Constants.DATAINFO_CONFIRM_QE);
+    }
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(PangolinReportApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "SwaggerUI: \thttp://localhost:{}/swagger-ui.html\n" +
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                env.getProperty("server.port"));
+    }
+}
