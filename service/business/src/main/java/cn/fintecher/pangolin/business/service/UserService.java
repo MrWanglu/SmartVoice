47e0d9766f3cc762401ebab80d6ea17ed130af0a
hukaijia <hukaijia>
2017-08-09 18:52:22 +0800

    增加系统日志的添加

--- service/business/src/main/java/cn/fintecher/pangolin/business/service/UserService.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/service/UserService.java
@@ -3,9 +3,11 @@ package cn.fintecher.pangolin.business.service;
 import cn.fintecher.pangolin.business.model.UserDeviceReset;
 import cn.fintecher.pangolin.business.repository.DepartmentRepository;
 import cn.fintecher.pangolin.business.repository.UserRepository;
+import cn.fintecher.pangolin.business.session.SessionStore;
 import cn.fintecher.pangolin.entity.Department;
 import cn.fintecher.pangolin.entity.QUser;
 import cn.fintecher.pangolin.entity.User;
+import cn.fintecher.pangolin.entity.util.Constants;
 import cn.fintecher.pangolin.entity.UserDevice;
 import com.querydsl.core.BooleanBuilder;
 import org.apache.commons.collections4.IteratorUtils;
@@ -14,6 +16,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 import java.util.*;
 
 /**
@@ -52,7 +59,7 @@ public class UserService {
     /**
      * @Description : 得到外访的主管 type 用户类型   state 用户的状态   companyCode 公司code   manager 是否管理者  0 是  1 否
      */
-    public List<User> getAllUser(String companyCode, Integer type, Integer state,Integer manager) {
+    public List<User> getAllUser(String companyCode, Integer type, Integer state, Integer manager) {
         QUser qUser = QUser.user;
         BooleanBuilder builder = new BooleanBuilder();
         if (Objects.nonNull(companyCode)) {
@@ -70,6 +77,17 @@ public class UserService {
         Iterator<User> userList = userRepository.findAll(builder).iterator();
         List<User> userReturn = IteratorUtils.toList(userList);
         return userReturn;
+    }
+    /**
+     * @Description : 通过token查询用户
+     */
+    public User getUserByToken(String token) throws Exception {
+        HttpSession session = SessionStore.getInstance().getSession(token);
+        if (session == null) {
+            throw new Exception(Constants.SYS_EXCEPTION_NOSESSION);
+        }
+        User user = (User) session.getAttribute(Constants.SESSION_USER);
+        return user;
     }
 
     /**
