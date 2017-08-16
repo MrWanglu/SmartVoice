0e7f7360fba6e89360b0dbfdeec649e2296241c1
xiaqun <qun.xia@zzlgroup.com>
2017-08-16 20:51:41 +0800

    修改了代码问题

--- service/business/src/main/java/cn/fintecher/pangolin/business/model/PayApplyParams.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/model/PayApplyParams.java
@@ -16,7 +16,7 @@ import java.util.List;
 @Data
 public class PayApplyParams {
     @DecimalMax(value = "9999999999.99", message = "减免金额不能大于100亿")
-    private BigDecimal derateFee;//减免费用
+    private BigDecimal derateFee = new BigDecimal(0);//减免费用
     private String derateDescripton;//减免备注
     private List<String> fileIds;//上传凭证Id集合
     @NotNull
@@ -26,7 +26,7 @@ public class PayApplyParams {
     private String caseId;//案件ID
     @NotNull
     @DecimalMax(value = "9999999999.99", message = "还款金额不能大于100亿")
-    private BigDecimal payAmt;//还款金额
+    private BigDecimal payAmt = new BigDecimal(0);//还款金额
     private String payDescripton;  //还款说明
     private Integer derateFlag; //减免标识 0-没有减免 1-有减免
 }
