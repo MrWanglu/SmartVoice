52bcdccf994177c238f7f0d7b20e8df08d4afecf
hukaijia <hukaijia>
2017-08-25 13:53:41 +0800

    修改跟进记录

--- service/business/src/main/java/cn/fintecher/pangolin/business/model/CaseFollowupParams.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/model/CaseFollowupParams.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Data
 public class CaseFollowupParams {
+    private String id;
     private String caseId; //案件ID
     private String personalId; //客户信息ID
     private Integer collectionFeedback; //催收反馈
