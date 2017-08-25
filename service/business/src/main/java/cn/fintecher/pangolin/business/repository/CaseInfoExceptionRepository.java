0ae5bec385d760deaf378b19bfbe2e6d2734e493
sunyanping <sunyanping24@sina.com>
2017-08-25 14:31:44 +0800

    异常案件Repository

--- service/business/src/main/java/cn/fintecher/pangolin/business/repository/CaseInfoExceptionRepository.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/repository/CaseInfoExceptionRepository.java
@@ -3,10 +3,14 @@ package cn.fintecher.pangolin.business.repository;
 
 import cn.fintecher.pangolin.entity.CaseInfoException;
 import cn.fintecher.pangolin.entity.QCaseInfoException;
+import com.querydsl.core.types.dsl.StringPath;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
+
+import java.math.BigDecimal;
+import java.util.Iterator;
 
 /**
  * Created by ChenChang on 2017/7/11.
@@ -15,10 +19,11 @@ public interface CaseInfoExceptionRepository extends QueryDslPredicateExecutor<C
     @Override
     default void customize(final QuerydslBindings bindings, final QCaseInfoException root) {
 
-        /*bindings.bind(String.class).first((StringPath path, String value) -> path.like(value));
-        bindings.bind(root.product.prodcutName).first((path,value) -> path.eq(value));
-        //机构码搜索
-        bindings.bind(root.department.code).first((path, value) -> path.startsWith(value));
+        bindings.bind(String.class).first((StringPath path, String value) -> path.like("%".concat(value).concat("%")));
+        //客户手机号
+        bindings.bind(root.mobileNo).first((path, value) -> path.eq(value));
+        //批次号
+        bindings.bind(root.batchNumber).first((path, value) -> path.eq(value));
         //公司码
         bindings.bind(root.companyCode).first((path, value) -> path.eq(value));
         //案件金额
@@ -34,7 +39,7 @@ public interface CaseInfoExceptionRepository extends QueryDslPredicateExecutor<C
             }
         });
         //逾期天数
-        bindings.bind(root.overdueDays).all((path, value) -> {
+        bindings.bind(root.overDueDays).all((path, value) -> {
             Iterator<? extends Integer> it=value.iterator();
             Integer firstOverdueDays=it.next();
             if(it.hasNext()){
@@ -45,15 +50,8 @@ public interface CaseInfoExceptionRepository extends QueryDslPredicateExecutor<C
             }
         });
         //委托方
-        bindings.bind(root.principalId.id).first((path, value) -> path.eq(value));
-        //案件状态
-        bindings.bind(root.collectionStatus).first((path, value) -> path.eq(value));
-        //案件类型
-        bindings.bind(root.caseType).first((path, value) -> path.eq(value));
-        //催收类型
-        bindings.bind(root.collectionType).first((path, value) -> path.eq(value));
-        //产品系列
-        bindings.bind(root.product.productSeries.id).first((path, value) -> path.eq(value));*/
+        bindings.bind(root.prinName).first((path, value) -> path.eq(value));
+
     }
 
 }
