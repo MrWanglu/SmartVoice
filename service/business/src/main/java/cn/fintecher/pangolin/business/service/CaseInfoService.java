0522287f213167e7ed4511eae4608b6a5835f1ce
xiaqun <qun.xia@zzlgroup.com>
2017-08-29 15:46:44 +0800

    修改了代码问题

--- service/business/src/main/java/cn/fintecher/pangolin/business/service/CaseInfoService.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/service/CaseInfoService.java
@@ -689,7 +689,9 @@ public class CaseInfoService {
                     caseTurnRecord.setReceiveUserRealName(batchInfoModel.getCollectionUser().getRealName()); //接受人名称
                     caseTurnRecord.setReceiveDeptName(batchInfoModel.getCollectionUser().getDepartment().getName()); //接收部门名称
                     caseTurnRecord.setReceiveUserId(caseInfo.getCurrentCollector().getId()); //接受人ID
-                    caseTurnRecord.setCurrentCollector(caseInfo.getLatelyCollector().getId()); //当前催收员ID
+                    if (Objects.nonNull(caseInfo.getLatelyCollector())) {
+                        caseTurnRecord.setCurrentCollector(caseInfo.getLatelyCollector().getId()); //当前催收员ID
+                    }
                     caseTurnRecord.setCirculationType(2); //流转类型 2-正常流转
                     caseTurnRecord.setOperatorUserName(tokenUser.getUserName()); //操作员用户名
                     caseTurnRecord.setOperatorTime(ZWDateUtil.getNowDateTime()); //操作时间
