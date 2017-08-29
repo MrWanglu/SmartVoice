b774e55b7d064f2ab23ad2b3b256a055032193b2
gaobeibei <572192995@qq.com>
2017-08-29 21:28:29 +0800

    APP待催收查询

--- service/business/src/main/java/cn/fintecher/pangolin/business/webapp/CaseFollowupAppController.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/webapp/CaseFollowupAppController.java
@@ -24,6 +24,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import javax.inject.Inject;
 import java.util.*;
 
@@ -57,20 +58,24 @@ public class CaseFollowupAppController extends BaseController {
 
     @GetMapping("/getAllFollowupsForApp")
     @ApiOperation(value = "APP查询案件跟进记录", notes = "APP查询案件跟进记录")
-    public ResponseEntity<Page<CaseFollowupRecord>> getAllFollowupsForApp(@ApiParam(value = "案件ID", required = true) @RequestParam String id, Pageable pageable) throws Exception{
-        BooleanBuilder builder = new BooleanBuilder();
-        builder.and(QCaseFollowupRecord.caseFollowupRecord.caseId.eq(id));
-        Page<CaseFollowupRecord> page = caseFollowupRecordRepository.findAll(builder, pageable);
-        for(CaseFollowupRecord caseFollowupRecord : page){
-            Iterable<CaseFlowupFile> iterable = caseFlowupFileRepository.findAll(new BooleanBuilder().and(QCaseFlowupFile.caseFlowupFile.followupId.id.eq(caseFollowupRecord.getId())));
-            List<String> fileIds = new ArrayList<>();
-            iterable.forEach(e->{
-                fileIds.add(e.getFileid());
-            });
-            caseFollowupRecord.setFileIds(fileIds);
+    public ResponseEntity<Page<CaseFollowupRecord>> getAllFollowupsForApp(@ApiParam(value = "案件ID", required = true) @RequestParam String id, Pageable pageable) throws Exception {
+        try {
+            BooleanBuilder builder = new BooleanBuilder();
+            builder.and(QCaseFollowupRecord.caseFollowupRecord.caseId.eq(id));
+            Page<CaseFollowupRecord> page = caseFollowupRecordRepository.findAll(builder, pageable);
+            for (CaseFollowupRecord caseFollowupRecord : page) {
+                Iterable<CaseFlowupFile> iterable = caseFlowupFileRepository.findAll(new BooleanBuilder().and(QCaseFlowupFile.caseFlowupFile.followupId.id.eq(caseFollowupRecord.getId())));
+                List<String> fileIds = new ArrayList<>();
+                iterable.forEach(e -> {
+                    fileIds.add(e.getFileid());
+                });
+                caseFollowupRecord.setFileIds(fileIds);
+            }
+            HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/CaseFollowupAppController");
+            return new ResponseEntity<>(page, headers, HttpStatus.OK);
+        }catch(Exception e){
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("", "CaseFollowupRecord", "查询失败")).body(null);
         }
-        HttpHeaders headers = PaginationUtil.generatePaginationHttpHeaders(page, "/api/CaseFollowupAppController");
-        return new ResponseEntity<>(page, headers, HttpStatus.OK);
     }
 
     @GetMapping("/getPayPromiseInfo")
@@ -84,7 +89,7 @@ public class CaseFollowupAppController extends BaseController {
             return ResponseEntity.ok().headers(HeaderUtil.createAlert("查询成功", "CaseFollowupRecord")).body(iterator.next());
         } catch (Exception e) {
             log.error(e.getMessage(), e);
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("CaseFollowupRecord", "CaseFollowupRecord", "")).body(null);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("CaseFollowupRecord", "CaseFollowupRecord", "查询失败")).body(null);
         }
     }
 
@@ -103,36 +108,40 @@ public class CaseFollowupAppController extends BaseController {
             log.debug(e.getMessage());
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(null, "Userexists", e.getMessage())).body(null);
         }
-        CaseInfo caseInfo = caseInfoRepository.findOne(caseFollowupParams.getCaseId());
-        CaseAssist one = caseAssistRepository.findOne(QCaseAssist.caseAssist.caseId.id.eq(caseFollowupParams.getCaseId())
-                .and(QCaseAssist.caseAssist.assistStatus.eq(CaseInfo.AssistStatus.ASSIST_WAIT_ACC.getValue())));
-        if (Objects.nonNull(one)) {
-            one.setAssistStatus(CaseInfo.AssistStatus.ASSIST_COLLECTING.getValue());
-            caseAssistRepository.save(one);
-        }
-        if(Objects.equals(caseInfo.getAssistFlag(),CaseInfo.AssistFlag.NO_ASSIST.getValue())){
-            caseFollowupParams.setSource(CaseFollowupRecord.Source.VISIT.getValue());
-            caseFollowupParams.setType(CaseFollowupRecord.Type.VISIT.getValue());
-        }else{
-            caseFollowupParams.setSource(CaseFollowupRecord.Source.ASSIST.getValue());
-            caseFollowupParams.setType(CaseFollowupRecord.Type.ASSIST.getValue());
-        }
-        CaseFollowupRecord result = caseInfoService.saveFollowupRecord(caseFollowupParams, user);
-        if(Objects.nonNull(caseFollowupParams.getFileIds())){
-            List<String> fileIds = caseFollowupParams.getFileIds();
-            Iterator<String> iterator = fileIds.iterator();
-            while(iterator.hasNext()){
-                CaseFlowupFile caseFlowupFile = new CaseFlowupFile();
-                caseFlowupFile.setFollowupId(result);
-                caseFlowupFile.setCaseId(caseInfo.getId());
-                caseFlowupFile.setCaseNumber(caseInfo.getCaseNumber());
-                caseFlowupFile.setFileid(iterator.next());
-                caseFlowupFile.setOperator(user.getId());
-                caseFlowupFile.setOperatorName(user.getUserName());
-                caseFlowupFile.setOperatorTime(ZWDateUtil.getNowDateTime());
-                caseFlowupFileRepository.save(caseFlowupFile);
+        try {
+            CaseInfo caseInfo = caseInfoRepository.findOne(caseFollowupParams.getCaseId());
+            CaseAssist one = caseAssistRepository.findOne(QCaseAssist.caseAssist.caseId.id.eq(caseFollowupParams.getCaseId())
+                    .and(QCaseAssist.caseAssist.assistStatus.eq(CaseInfo.AssistStatus.ASSIST_WAIT_ACC.getValue())));
+            if (Objects.nonNull(one)) {
+                one.setAssistStatus(CaseInfo.AssistStatus.ASSIST_COLLECTING.getValue());
+                caseAssistRepository.save(one);
             }
+            if (Objects.equals(caseInfo.getAssistFlag(), CaseInfo.AssistFlag.NO_ASSIST.getValue())) {
+                caseFollowupParams.setSource(CaseFollowupRecord.Source.VISIT.getValue());
+                caseFollowupParams.setType(CaseFollowupRecord.Type.VISIT.getValue());
+            } else {
+                caseFollowupParams.setSource(CaseFollowupRecord.Source.ASSIST.getValue());
+                caseFollowupParams.setType(CaseFollowupRecord.Type.ASSIST.getValue());
+            }
+            CaseFollowupRecord result = caseInfoService.saveFollowupRecord(caseFollowupParams, user);
+            if (Objects.nonNull(caseFollowupParams.getFileIds())) {
+                List<String> fileIds = caseFollowupParams.getFileIds();
+                Iterator<String> iterator = fileIds.iterator();
+                while (iterator.hasNext()) {
+                    CaseFlowupFile caseFlowupFile = new CaseFlowupFile();
+                    caseFlowupFile.setFollowupId(result);
+                    caseFlowupFile.setCaseId(caseInfo.getId());
+                    caseFlowupFile.setCaseNumber(caseInfo.getCaseNumber());
+                    caseFlowupFile.setFileid(iterator.next());
+                    caseFlowupFile.setOperator(user.getId());
+                    caseFlowupFile.setOperatorName(user.getUserName());
+                    caseFlowupFile.setOperatorTime(ZWDateUtil.getNowDateTime());
+                    caseFlowupFileRepository.save(caseFlowupFile);
+                }
+            }
+            return ResponseEntity.ok().headers(HeaderUtil.createAlert("添加跟进记录成功", "CaseFollowupRecord")).body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("", "CaseFollowupRecord", "添加失败")).body(null);
         }
-        return ResponseEntity.ok().headers(HeaderUtil.createAlert("添加跟进记录成功", "CaseFollowupRecord")).body(result);
     }
 }
