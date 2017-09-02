aabf83f03127fe7c4cbc4396b21ff1441613274b
gaobeibei <572192995@qq.com>
2017-09-02 15:48:15 +0800

    极光推送

---
+++ service/business/src/main/java/cn/fintecher/pangolin/business/model/CapaPersons.java
@@ -0,0 +1,15 @@
+package cn.fintecher.pangolin.business.model;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/9/1.
+ */
+@Data
+public class CapaPersons {
+    List<String> contId;
+    List<String> personalName;
+    List<String> personalPhone;
+}
---
+++ service/business/src/main/java/cn/fintecher/pangolin/business/service/MessageService.java
@@ -0,0 +1,48 @@
+package cn.fintecher.pangolin.business.service;
+
+import cn.fintecher.pangolin.business.model.PersonModel;
+import cn.fintecher.pangolin.business.model.SMSMessageParams;
+import cn.fintecher.pangolin.business.model.SendMessageModel;
+import cn.fintecher.pangolin.business.repository.CaseInfoRepository;
+import cn.fintecher.pangolin.business.repository.PersonalContactRepository;
+import cn.fintecher.pangolin.business.repository.PersonalRepository;
+import cn.fintecher.pangolin.business.repository.SendMessageRecordRepository;
+import cn.fintecher.pangolin.entity.*;
+import cn.fintecher.pangolin.util.ZWDateUtil;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Administrator on 2017/9/1.
+ */
+@Service("messageService")
+public class MessageService {
+    @Inject
+    PersonalContactRepository personalContactRepository;
+    @Inject
+    SendMessageRecordRepository sendMessageRecordRepository;
+
+    public SendMessageRecord saveMessage(CaseInfo caseInfo, Personal personal, Template template, String id, User user, Integer way) {
+        SendMessageRecord sendMessageRecord = new SendMessageRecord();
+        sendMessageRecord.setPersonalId(personal.getId());
+        sendMessageRecord.setPersonalName(personal.getName());
+        sendMessageRecord.setTempelateType(template.getTemplateType());
+        sendMessageRecord.setTempelateName(template.getTemplateName());
+        sendMessageRecord.setTempelateCode(template.getTemplateCode());
+        sendMessageRecord.setTempelateId(template.getId());
+        sendMessageRecord.setCompanyCode(user.getCompanyCode());
+        sendMessageRecord.setOperatorUserName(user.getUserName());
+        sendMessageRecord.setOperatorRealName(user.getRealName());
+        sendMessageRecord.setOperatorDate(ZWDateUtil.getNowDateTime());
+        sendMessageRecord.setMessageContent(template.getMessageContent());
+        sendMessageRecord.setSendWay(way);
+        sendMessageRecord.setCaseId(caseInfo.getId());
+        PersonalContact personalContact = personalContactRepository.getOne(id);
+        sendMessageRecord.setTarget(personalContact.getRelation());
+        sendMessageRecord.setTargetName(personalContact.getName());
+        sendMessageRecord.setPersonalContactId(id);
+        SendMessageRecord result = sendMessageRecordRepository.save(sendMessageRecord);
+        return result;
+    }
+}
