effc45d7f60a1312f4463fe3668ce4c65ceb4a93
hukaijia <hukaijia>
2017-09-08 09:48:56 +0800

    添加云羿呼叫中心的心跳调度

---
+++ service/common/src/main/java/cn/fintecher/pangolin/common/scheduled/RecordDayScheduled.java
@@ -0,0 +1,62 @@
+package cn.fintecher.pangolin.common.scheduled;
+
+import cn.fintecher.pangolin.entity.util.Constants;
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Map;
+
+/**
+ * Created by  hukaijia.
+ * Description:
+ * Date: 2017-07-05-11:36
+ */
+@Component
+@EnableScheduling
+@Lazy(value = false)
+public class RecordDayScheduled {
+    private final Logger log = LoggerFactory.getLogger(RecordDayScheduled.class);
+
+    @Scheduled(cron = "0/59 * * * * ?")
+    void callHeartBeat() throws IOException {
+        log.info("发送心跳" + new DateTime().toString("yyyy-MM-dd HH:mm:ss"));
+        try {
+            Map<String,String> map= Constants.map;
+            for(String value : map.values()){
+                Socket socket = new Socket("116.236.220.211", 12345);
+                socket.setSoTimeout(10000000);
+                BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
+                String sendData = "<request><cmdType>heartbeat</cmdType><agentID>"+value+"</agentID></request>";
+                String sendDataUtf82 = new String(sendData.getBytes("UTF-8"), "UTF-8");
+                String head2 = "<<<length=" + sendDataUtf82.getBytes("UTF-8").length + ">>>";
+                sendDataUtf82 = head2 + sendDataUtf82;
+                System.out.println("心跳：" + sendDataUtf82);
+                pw.print(sendDataUtf82);
+                pw.flush();
+            }
+
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Scheduled(cron = "* 0/30 0/23 0/1 * ?")
+    void resetMap() throws IOException {
+        log.info("清除Map" + new DateTime().toString("yyyy-MM-dd HH:mm:ss"));
+        try {
+            Map<String,String> map= Constants.map;
+            map.clear();
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+}
