f25818ca6732706b05134cee6c01b9950eae4aab
james <457237252@qq.com>
2017-09-15 16:47:06 +0800

    修改委托方修改的错误。

--- service/business/src/main/java/cn/fintecher/pangolin/business/web/PrincipalController.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/web/PrincipalController.java
@@ -222,7 +222,7 @@ public class PrincipalController extends BaseController {
             Principal principal = principalRepository.findOne(request.getId());
             //验证委外方是否重名
             QPrincipal qPrincipal = QPrincipal.principal;
-            Iterator<Principal> principalIterator = principalRepository.findAll(qPrincipal.name.eq(request.getName()).and(qPrincipal.flag.eq(Outsource.deleteStatus.START.getDeleteCode())).and(qPrincipal.id.ne(request.getId()))).iterator();
+            Iterator<Principal> principalIterator = principalRepository.findAll(qPrincipal.name.eq(request.getName()).and(qPrincipal.flag.eq(Outsource.deleteStatus.START.getDeleteCode())).and(qPrincipal.id.ne(request.getId())).and(qPrincipal.companyCode.eq(request.getCompanyCode()))).iterator();
             if (principalIterator.hasNext()) {
                 return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME,
                         "The outsourcename is not allowed to be used", "该名字不允许被使用")).body(null);
