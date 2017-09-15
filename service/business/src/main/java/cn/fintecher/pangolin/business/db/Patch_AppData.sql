1e4808f9d572b2a0b4c80c50a5ab249f9be80537
zejie0216 <543118962@qq.com>
2017-09-15 18:43:15 +0800

    添加权限码

--- service/business/src/main/java/cn/fintecher/pangolin/business/db/Patch_AppData.sql
+++ service/business/src/main/java/cn/fintecher/pangolin/business/db/Patch_AppData.sql
@@ -8,3 +8,8 @@ INSERT INTO `pangolin_business`.`data_dict` (`id`, `type_code`, `code`, `name`, 
 
 --2017-09-14 huyanmin 增加公司序列号 sequence
 ALTER TABLE company ADD sequence VARCHAR(4) DEFAULT NULL COMMENT '公司序列号';
+
+--2017-09-15 huyanmin 增加新的权限码
+INSERT INTO `resource` VALUES ('769', '176', '催大人', '公司名称', '080108', NULL, NULL, NULL, NULL, NULL, 19, NULL, NULL, NULL, NULL, 769);
+INSERT INTO `resource` VALUES ('770', '239', '催大人', '批量管理', '090506', NULL, NULL, NULL, NULL, NULL, 19, NULL, NULL, NULL, NULL, 770);
+INSERT INTO `resource` VALUES ('771', '243', '催大人', '公司名称', '080206', NULL, NULL, NULL, NULL, NULL, 19, NULL, NULL, NULL, NULL, 771);
