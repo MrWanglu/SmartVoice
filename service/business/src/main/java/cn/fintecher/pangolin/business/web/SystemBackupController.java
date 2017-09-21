5f6c1f1650e1546d3ad427d6ca7f5afd870679e6
hukaijia <hukaijia>
2017-09-21 13:04:05 +0800

    新增数据备份服务

---
+++ service/business/src/main/java/cn/fintecher/pangolin/business/web/SystemBackupController.java
@@ -0,0 +1,55 @@
+package cn.fintecher.pangolin.business.web;
+
+import cn.fintecher.pangolin.business.repository.SystemBackupRepository;
+import cn.fintecher.pangolin.entity.SystemBackup;
+import cn.fintecher.pangolin.entity.User;
+import cn.fintecher.pangolin.entity.util.EntityUtil;
+import cn.fintecher.pangolin.web.HeaderUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by  hukaijia.
+ * Description:
+ * Date: 2017-09-21-9:21
+ */
+@RestController
+@RequestMapping("/api/systemBackupController")
+@Api(value = "SystemBackupController", description = "系统数据库备份")
+public class SystemBackupController extends BaseController {
+    private final Logger logger = LoggerFactory.getLogger(SystemBackupController.class);
+    private static final String ENTITY_NAME = "SystemBackup";
+
+    @Autowired
+    private SystemBackupRepository systemBackupRepository;
+
+    /**
+     * @Description : 新增系统数据库备份
+     */
+    @PostMapping("/createSystemBackup")
+    @ApiOperation(value = "增加系统数据库备份", notes = "增加系统数据库备份")
+    public ResponseEntity<SystemBackup> createSystemBackup(@RequestBody SystemBackup request,
+                                                           @RequestHeader(value = "X-UserToken") String token) {
+        request = (SystemBackup) EntityUtil.emptyValueToNull(request);
+        logger.debug("REST request to save caseInfo : {}", request);
+        if (request.getId() != null) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME,
+                    "idexists", "新增不应该含有ID")).body(null);
+        }
+        User user;
+        try {
+            user = getUserByToken(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "User is not login", "用户未登录")).body(null);
+        }
+        //增加系统数据库备份
+        SystemBackup systemBackup = systemBackupRepository.save(request);
+        return ResponseEntity.ok().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "operate successfully", "操作成功")).body(systemBackup);
+    }
+}
