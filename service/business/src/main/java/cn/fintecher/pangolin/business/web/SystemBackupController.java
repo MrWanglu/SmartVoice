8e0d19906ce8df9552a76f2288d95269a6d2fd7f
hukaijia <hukaijia>
2017-09-22 11:07:36 +0800

    优化新增数据备份服务 手动的

--- service/business/src/main/java/cn/fintecher/pangolin/business/web/SystemBackupController.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/web/SystemBackupController.java
@@ -2,24 +2,27 @@ package cn.fintecher.pangolin.business.web;
 
 import cn.fintecher.pangolin.business.repository.SysParamRepository;
 import cn.fintecher.pangolin.business.repository.SystemBackupRepository;
-import cn.fintecher.pangolin.entity.QSysParam;
-import cn.fintecher.pangolin.entity.SysParam;
-import cn.fintecher.pangolin.entity.SystemBackup;
-import cn.fintecher.pangolin.entity.User;
+import cn.fintecher.pangolin.business.service.SystemBackupService;
+import cn.fintecher.pangolin.entity.*;
 import cn.fintecher.pangolin.entity.util.Constants;
 import cn.fintecher.pangolin.entity.util.EntityUtil;
+import cn.fintecher.pangolin.util.ZWDateUtil;
 import cn.fintecher.pangolin.web.HeaderUtil;
+import com.querydsl.core.BooleanBuilder;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Date;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -40,6 +43,58 @@ public class SystemBackupController extends BaseController {
     private SystemBackupRepository systemBackupRepository;
     @Autowired
     private SysParamRepository sysParamRepository;
+    @Autowired
+    private SystemBackupService systemBackupService;
+
+    /**
+     * @Description : 查询系统备份
+     */
+
+    @GetMapping("/querySystemBackup")
+    @ApiOperation(value = "查询系统备份", notes = "查询系统备份")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", dataType = "int", paramType = "query",
+                    value = "页数 (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "int", paramType = "query",
+                    value = "每页大小."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
+                    value = "依据什么排序: 属性名(,asc|desc). ")
+    })
+    public ResponseEntity<Page<SystemBackup>> querySystemBackup(@RequestParam(required = false) String companyCode,
+                                                                @RequestParam(required = false) Integer type,
+                                                                @RequestParam(required = false) String mysqlName,
+                                                                @RequestParam(required = false) String mongdbName,
+                                                                @RequestParam(required = false) String operator,
+                                                                @RequestParam(required = false) Date operateTime,
+                                                                @ApiIgnore Pageable pageable,
+                                                                @RequestHeader(value = "X-UserToken") String token) {
+        User user;
+        try {
+            user = getUserByToken(token);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "User is not login", "用户未登录")).body(null);
+        }
+        QSystemBackup qSystemBackup = QSystemBackup.systemBackup;
+        BooleanBuilder builder = new BooleanBuilder();
+        if (Objects.nonNull(companyCode)) {
+            builder.and(qSystemBackup.companyCode.eq(companyCode));
+        }
+        if (Objects.nonNull(type)) {
+            builder.and(qSystemBackup.type.eq(type));
+        }
+        if (Objects.nonNull(mysqlName)) {
+            builder.and(qSystemBackup.mysqlName.like(mysqlName.concat("%")));
+        }
+        if (Objects.nonNull(mongdbName)) {
+            builder.and(qSystemBackup.mongdbName.like(mongdbName.concat("%")));
+        }
+        if (Objects.nonNull(operator)) {
+            builder.and(qSystemBackup.operator.like(operator.concat("%")));
+        }
+        Page<SystemBackup> page = systemBackupRepository.findAll(builder, pageable);
+        return ResponseEntity.ok().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "operate successfully", "操作成功")).body(page);
+    }
 
     /**
      * @Description : 新增系统数据库备份
@@ -68,44 +123,16 @@ public class SystemBackupController extends BaseController {
             e.printStackTrace();
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "exception for parameters", "系统数据库备份地址参数异常")).body(null);
         }
-        //调用shell脚本备份mysql数据库
-        BufferedReader br = null;
-        Process ps = null;
-        String result = null;
-        try {
-            logger.info("开始备份");
-            String[] shpath = {sysParams.getValue(), user.getUserName()};
-            ps = Runtime.getRuntime().exec(shpath);
-            ps.waitFor();
-            br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
-            StringBuffer sb = new StringBuffer();
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            result = sb.toString();
-            Pattern p = Pattern.compile(".*sql");
-            Matcher m = p.matcher(result);
-            while (!(m.find())) {
-                return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "Mysql database backup failed", "mysql数据库备份失败")).body(null);
-            }
-//            System.out.println(result);
-            logger.info("备份返回值" + result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (ps != null) {
-                    ps.destroy();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        String result = systemBackupService.operationShell(sysParams.getValue(), user.getUserName());
+        Pattern p = Pattern.compile(".*sql");
+        Matcher m = p.matcher(result);
+        while (!(m.find())) {
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "Mysql database backup failed", "mysql数据库备份失败")).body(null);
         }
+        request.setType(1);
         request.setMysqlName(result);
+        request.setOperator(user.getRealName());
+        request.setOperateTime(ZWDateUtil.getNowDateTime());
         //增加系统数据库备份
         SystemBackup systemBackup = systemBackupRepository.save(request);
         return ResponseEntity.ok().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "operate successfully", "操作成功")).body(systemBackup);
@@ -137,37 +164,9 @@ public class SystemBackupController extends BaseController {
             e.printStackTrace();
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "exception for parameters", "恢复系统数据库地址参数异常")).body(null);
         }
-        //调用shell脚本备份数据库
-        BufferedReader br = null;
-        Process ps = null;
-        String result = null;
-        try {
-            logger.info("开始恢复备份");
-            String[] shpath = {sysParams.getValue(), request.getMysqlName()};
-            ps = Runtime.getRuntime().exec(shpath);
-            ps.waitFor();
-            br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
-            StringBuffer sb = new StringBuffer();
-            String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-            result = sb.toString();
-//            System.out.println(result);
-            logger.info("恢复备份返回值" + result);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (ps != null) {
-                    ps.destroy();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        String result = systemBackupService.operationShell(sysParams.getValue(), request.getMysqlName());
+        if(!Objects.equals("success",result)){
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "fail to backup mysql database", "mysql数据库备份失败")).body(null);
         }
         return ResponseEntity.ok().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "operate successfully", "操作成功")).body(result);
     }
