6a2b0f86e5b3e760761e28b0ea8b9cc53ff9b4d1
yuanyanting <tingyanyuan821@163.com>
2017-09-27 16:20:27 +0800

    修改了外访查询。

--- service/business/src/main/java/cn/fintecher/pangolin/business/service/CaseInfoVerificationService.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/service/CaseInfoVerificationService.java
@@ -6,10 +6,9 @@ import cn.fintecher.pangolin.business.model.CaseInfoVerificationParams;
 import cn.fintecher.pangolin.business.repository.CaseInfoVerificationRepository;
 import cn.fintecher.pangolin.business.repository.DataDictRepository;
 import cn.fintecher.pangolin.business.utils.ExcelExportHelper;
-import cn.fintecher.pangolin.entity.DataDict;
-import cn.fintecher.pangolin.entity.QDataDict;
-import cn.fintecher.pangolin.entity.User;
+import cn.fintecher.pangolin.entity.*;
 import cn.fintecher.pangolin.entity.util.ExcelUtil;
+import cn.fintecher.pangolin.util.ZWDateUtil;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -17,6 +16,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -286,6 +286,39 @@ public class CaseInfoVerificationService {
             return String.valueOf(obj.equals("null") ? "" : obj);
         } else {
             return null;
+        }
+    }
+
+    /**
+     * set核销申请属性值
+     * @param apply 核销申请
+     * @param caseInfo 案件
+     * @param user 申请人
+     * @param applyReason
+     */
+    public void setVerificationApply(CaseInfoVerificationApply apply, CaseInfo caseInfo, User user, String applyReason) {
+        BeanUtils.copyProperties(caseInfo, apply);
+        apply.setId(null);
+        /*apply.setOperator(user.getRealName()); // 操作人
+        apply.setOperatorTime(ZWDateUtil.getNowDateTime()); // 操作时间*/
+        apply.setApplicant(user.getRealName()); // 申请人
+        apply.setApplicationDate(ZWDateUtil.getNowDateTime()); // 申请日期
+        apply.setApplicationReason(applyReason); // 申请理由
+        apply.setApprovalStatus(CaseInfoVerificationApply.ApprovalStatus.approval_pending.getValue()); // 申请状态：审批待通过
+        apply.setCaseId(caseInfo.getId()); // 案件Id
+        if (Objects.nonNull(caseInfo.getArea())) {
+            apply.setCityId(caseInfo.getArea().getId()); // 城市
+            if (Objects.nonNull(caseInfo.getArea().getParent())) {
+                apply.setProvinceId(caseInfo.getArea().getParent().getId()); // 省份
+            }
+        }
+        if (Objects.nonNull(caseInfo.getPrincipalId())) {
+            apply.setPrincipalName(caseInfo.getPrincipalId().getName()); // 委托方名称
+        }
+        if (Objects.nonNull(caseInfo.getPersonalInfo())) {
+            apply.setPersonalName(caseInfo.getPersonalInfo().getName()); // 客户名称
+            apply.setMobileNo(caseInfo.getPersonalInfo().getMobileNo()); // 电话号
+            apply.setIdCard(caseInfo.getPersonalInfo().getIdCard()); // 身份证号
         }
     }
 }
