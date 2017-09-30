3270321cf8ba9bce2d4b18957469572dcde1541c
sunyanping <sunyanping24@sina.com>
2017-09-30 16:10:54 +0800

    案件策略

--- service/dataimp/src/main/java/cn/fintecher/pangolin/dataimp/repository/CaseStrategyRepository.java
+++ service/dataimp/src/main/java/cn/fintecher/pangolin/dataimp/repository/CaseStrategyRepository.java
@@ -2,9 +2,10 @@ package cn.fintecher.pangolin.dataimp.repository;
 
 import cn.fintecher.pangolin.entity.strategy.CaseStrategy;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 /**
  * Created by luqiang on 2017/8/2.
  */
-public interface CaseStrategyRepository extends MongoRepository<CaseStrategy, String> {
+public interface CaseStrategyRepository extends MongoRepository<CaseStrategy, String>,QueryDslPredicateExecutor<CaseStrategy> {
 }
