d1b3a14d9d1880f360c2ae0d30e8dbbce656e2ec
james <457237252@qq.com>
2017-10-11 22:33:21 +0800

    优化代码，增加日志打印。

--- service/report/src/main/java/cn/fintecher/pangolin/report/web/QueryOutsourcePoolController.java
+++ service/report/src/main/java/cn/fintecher/pangolin/report/web/QueryOutsourcePoolController.java
@@ -10,6 +10,8 @@ import com.github.pagehelper.PageInfo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,18 +31,19 @@ import java.util.List;
 @Api(value = "QueryOutsourcePoolController", description = "委外催收中查询")
 public class QueryOutsourcePoolController extends BaseController {
 
+    private final Logger log = LoggerFactory.getLogger(QueryOutsourcePoolController.class);
+
+
     @Inject
     QueryOutsourcePoolMapper queryOutsourcePoolMapper;
 
     @GetMapping("/queryAllOutsourcePool")
     @ApiOperation(value = "委外催收中查询", notes = "委外催收中查询")
-    public ResponseEntity<OutSourcePoolModel> queryAllOutsourcePool(@RequestParam(required = true)@ApiParam(value = "页数") Integer page,
-                                                                    @RequestParam(required = true)@ApiParam(value = "大小") Integer size,
-                                                                    QueryOutsourcePoolParams queryOutsourcePoolParams ){
-
+    public ResponseEntity<OutSourcePoolModel> queryAllOutsourcePool(@RequestParam @ApiParam(value = "页数") Integer page,
+                                                                    @RequestParam @ApiParam(value = "大小") Integer size,
+                                                                    QueryOutsourcePoolParams queryOutsourcePoolParams) {
         try {
-
-            PageHelper.startPage(page+1,size);
+            PageHelper.startPage(page + 1, size);
             List<QueryOutsourcePool> content = queryOutsourcePoolMapper.getAllOutSourcePoolModel(queryOutsourcePoolParams);
             PageInfo pageInfo = new PageInfo(content);
             OutSourcePoolModel outSourcePoolModel = new OutSourcePoolModel();
@@ -49,7 +52,8 @@ public class QueryOutsourcePoolController extends BaseController {
             outSourcePoolModel.setGetTotalElements(pageInfo.getTotal());
             return ResponseEntity.ok().body(outSourcePoolModel);
         } catch (Exception e) {
-           return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("QueryOutsourcePoolController", "queryAllOutsourcePool", e.getMessage())).body(null);
+            log.error(e.getMessage(), e);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("QueryOutsourcePoolController", "queryAllOutsourcePool", "委外催收中查询失败")).body(null);
         }
 
     }
