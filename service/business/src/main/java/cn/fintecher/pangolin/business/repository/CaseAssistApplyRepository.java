20942d66628c4d768bde7c09302687fd5e2dbbdb
xiaqun <qun.xia@zzlgroup.com>
2017-10-17 17:03:26 +0800

    修改了多条件查询还款记录接口

--- service/business/src/main/java/cn/fintecher/pangolin/business/repository/CaseAssistApplyRepository.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/repository/CaseAssistApplyRepository.java
@@ -2,6 +2,8 @@ package cn.fintecher.pangolin.business.repository;
 
 import cn.fintecher.pangolin.entity.CaseAssistApply;
 import cn.fintecher.pangolin.entity.QCaseAssistApply;
+import cn.fintecher.pangolin.util.ZWDateUtil;
+import com.querydsl.core.types.dsl.DateTimePath;
 import com.querydsl.core.types.dsl.StringPath;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +12,8 @@ import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
 
@@ -26,11 +30,18 @@ public interface CaseAssistApplyRepository extends QueryDslPredicateExecutor<Cas
         bindings.bind(String.class).first((StringPath path, String value) -> path.like("%".concat(StringUtils.trim(value)).concat("%")));
 //        bindings.bind(root.assistWay).first(SimpleExpression::eq); //协催方式
 //        bindings.bind(root.approveStatus).first(SimpleExpression::eq); //协催审批状态
-        bindings.bind(root.applyDate).all((path, value) -> { //申请时间
+        bindings.bind(root.applyDate).all((DateTimePath<Date> path, Collection<? extends Date> value) -> { //申请时间
             Iterator<? extends Date> it = value.iterator();
             Date applyMinDate = it.next();
             if (it.hasNext()) {
-                Date applyMaxDate = it.next();
+                String date = ZWDateUtil.fomratterDate(it.next(), "yyyy-MM-dd");
+                date = date + " 23:59:59";
+                Date applyMaxDate = null;
+                try {
+                    applyMaxDate = ZWDateUtil.getFormatDate(date);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
                 return path.between(applyMinDate, applyMaxDate);
             } else {
                 return path.goe(applyMinDate);
