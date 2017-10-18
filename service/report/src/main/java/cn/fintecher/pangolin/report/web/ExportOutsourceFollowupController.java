c443abad69d1270355ad868990e34228464ce26c
zejie0216 <543118962@qq.com>
2017-10-18 11:55:55 +0800

    通过消息队列导出跟进记录

--- service/report/src/main/java/cn/fintecher/pangolin/report/web/ExportOutsourceFollowupController.java
+++ service/report/src/main/java/cn/fintecher/pangolin/report/web/ExportOutsourceFollowupController.java
@@ -64,7 +64,7 @@ public class ExportOutsourceFollowupController extends BaseController {
         User user = null;
         try {
             user = getUserByToken(token);
-            if(Objects.nonNull(user.getCompanyCode())){
+            if (Objects.nonNull(user.getCompanyCode())) {
                 exportOutsourceFollowRecordParams.setCompanyCode(user.getCompanyCode());
             }
         } catch (final Exception e) {
@@ -92,131 +92,135 @@ public class ExportOutsourceFollowupController extends BaseController {
         exportOutsourceFollowRecordParams.setExportItemList(items);
         final String userId = user.getId();
         try {
-            String url = null;
-            SXSSFWorkbook workbook = null;
-            File file = null;
-            ByteArrayOutputStream out = null;
-            FileOutputStream fileOutputStream = null;
-            ProgressMessage progressMessage = null;
-            try {
-                progressMessage = new ProgressMessage();
-                // 登录人ID
-                progressMessage.setUserId(userId);
-                //要解析的总数据
-                progressMessage.setTotal(5);
-                //当前解析的数据
-                progressMessage.setCurrent(0);
-                //正在处理数据
-                progressMessage.setText("正在处理数据");
-                rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
-                List<ExcportOutsourceResultModel> all = null;
-                if(exportOutsourceFollowRecordParams.getType()==0){
-                    all = queryOutsourceFollowupMapper.findOutsourceRecord(exportOutsourceFollowRecordParams);
-                }else {
-                    all = queryOutsourceFollowupMapper.findOutsourceFollowupRecord(exportOutsourceFollowRecordParams);
-                }
-                if (!all.isEmpty()) {
-                    progressMessage.setCurrent(2);
+            //创建一个线程，执行导出任务
+            Thread t = new Thread(() -> {
+                SXSSFWorkbook workbook = null;
+                File file = null;
+                ByteArrayOutputStream out = null;
+                FileOutputStream fileOutputStream = null;
+                ProgressMessage progressMessage = null;
+                try {
+                    progressMessage = new ProgressMessage();
+                    // 登录人ID
+                    progressMessage.setUserId(userId);
+                    //要解析的总数据
+                    progressMessage.setTotal(5);
+                    //当前解析的数据
+                    progressMessage.setCurrent(0);
+                    //正在处理数据
+                    progressMessage.setText("正在处理数据");
                     rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
-                    int maxNum = outsourceFollowRecordExportService.getMaxNum(all);
-                    List<FollowupExportModel> dataList = null;
-                    if (exportOutsourceFollowRecordParams.getType()==0){
-                        dataList = outsourceFollowRecordExportService.getOutsourceRecordData(all);
+                    List<ExcportOutsourceResultModel> all = null;
+                    if(exportOutsourceFollowRecordParams.getType()==0){
+                        all = queryOutsourceFollowupMapper.findOutsourceRecord(exportOutsourceFollowRecordParams);
                     }else {
-                        dataList = outsourceFollowRecordExportService.getFollowupData(all);
+                        all = queryOutsourceFollowupMapper.findOutsourceFollowupRecord(exportOutsourceFollowRecordParams);
                     }
-                    String[] title = followRecordExportService.getTitle(exportOutsourceFollowRecordParams.getExportItemList(), maxNum);
-                    Map<String, String> headMap = ExcelExportUtil.createHeadMap(title, FollowupExportModel.class);
-                    progressMessage.setCurrent(3);
-                    rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
-                    workbook = new SXSSFWorkbook(5000);
-                    ExcelExportUtil.createExcelData(workbook, headMap, dataList, 1048575);
-                    out = new ByteArrayOutputStream();
-                    workbook.write(out);
-                    String filePath = FileUtils.getTempDirectoryPath().concat(File.separator).concat(DateTime.now().toString("yyyyMMddhhmmss") + "跟进记录.xlsx");
-                    file = new File(filePath);
-                    fileOutputStream = new FileOutputStream(file);
-                    fileOutputStream.write(out.toByteArray());
-                    FileSystemResource resource = new FileSystemResource(file);
-                    MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
-                    param.add("file", resource);
-                    ResponseEntity<String> stringResponseEntity = restTemplate.postForEntity("http://file-service/api/uploadFile/addUploadFileUrl", param, String.class);
-                    url = stringResponseEntity.getBody();
-                }
+                    ResponseEntity<String> url = null;
+                    if (!all.isEmpty()) {
+                        progressMessage.setCurrent(2);
+                        rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
+                        int maxNum = outsourceFollowRecordExportService.getMaxNum(all);
+                        List<FollowupExportModel> dataList = null;
+                        if (exportOutsourceFollowRecordParams.getType()==0){
+                            dataList = outsourceFollowRecordExportService.getOutsourceRecordData(all);
+                        }else {
+                            dataList = outsourceFollowRecordExportService.getFollowupData(all);
+                        }
+                        String[] title = followRecordExportService.getTitle(exportOutsourceFollowRecordParams.getExportItemList(), maxNum);
+                        Map<String, String> headMap = ExcelExportUtil.createHeadMap(title, FollowupExportModel.class);
+                        progressMessage.setCurrent(3);
+                        rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
+                        workbook = new SXSSFWorkbook(5000);
+                        ExcelExportUtil.createExcelData(workbook, headMap, dataList, 1048575);
+                        out = new ByteArrayOutputStream();
+                        workbook.write(out);
+                        String filePath = FileUtils.getTempDirectoryPath().concat(File.separator).concat(DateTime.now().toString("yyyyMMddhhmmss") + "跟进记录.xlsx");
+                        file = new File(filePath);
+                        fileOutputStream = new FileOutputStream(file);
+                        fileOutputStream.write(out.toByteArray());
+                        FileSystemResource resource = new FileSystemResource(file);
+                        MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
+                        param.add("file", resource);
+                        url = restTemplate.postForEntity(Constants.UPLOAD_FILE_URL, param, String.class);
+                    }
                     if (url == null && !all.isEmpty()) {
+                        List<String> urls = new ArrayList<>();
+                        ListResult listResult = new ListResult();
+                        listResult.setUser(userId);
+                        urls.add("导出失败！");
+                        listResult.setStatus(ListResult.Status.FAILURE.getVal()); // 1-失败
+                        restTemplate.postForEntity("http://reminder-service/api/listResultMessageResource", listResult, Void.class);
+                        progressMessage.setCurrent(5);
+                        rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
+                    } else {
+                        if (all.isEmpty()) {
+                            List<String> urls = new ArrayList<>();
+                            ListResult listResult = new ListResult();
+                            urls.add("要导出的数据为空");
+                            listResult.setUser(userId);
+                            listResult.setResult(urls);
+                            listResult.setStatus(ListResult.Status.FAILURE.getVal()); // 0-成功
+                            restTemplate.postForEntity("http://reminder-service/api/listResultMessageResource", listResult, Void.class);
+                            progressMessage.setCurrent(5);
+                            rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
+                        } else {
+                            List<String> urls = new ArrayList<>();
+                            ListResult listResult = new ListResult();
+                            urls.add(url.getBody());
+                            log.info(url.getBody());
+                            listResult.setUser(userId);
+                            listResult.setResult(urls);
+                            listResult.setStatus(ListResult.Status.SUCCESS.getVal()); // 0-成功
+                            restTemplate.postForEntity("http://reminder-service/api/listResultMessageResource", listResult, Void.class);
+                            progressMessage.setCurrent(5);
+                            rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                     List<String> urls = new ArrayList<>();
                     ListResult listResult = new ListResult();
-                    listResult.setUser(userId);
                     urls.add("导出失败！");
                     listResult.setStatus(ListResult.Status.FAILURE.getVal()); // 1-失败
                     restTemplate.postForEntity("http://reminder-service/api/listResultMessageResource", listResult, Void.class);
                     progressMessage.setCurrent(5);
                     rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
-                } else {
-                    if (all.isEmpty()) {
-                        List<String> urls = new ArrayList<>();
-                        ListResult listResult = new ListResult();
-                        urls.add("要导出的数据为空");
-                        listResult.setUser(userId);
-                        listResult.setResult(urls);
-                        listResult.setStatus(ListResult.Status.FAILURE.getVal()); // 0-成功
-                        restTemplate.postForEntity("http://reminder-service/api/listResultMessageResource", listResult, Void.class);
-                        progressMessage.setCurrent(5);
-                        rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
-                    } else {
-                        List<String> urls = new ArrayList<>();
-                        ListResult listResult = new ListResult();
-                        urls.add(url);
-                        log.info(url);
-                        listResult.setUser(userId);
-                        listResult.setResult(urls);
-                        listResult.setStatus(ListResult.Status.SUCCESS.getVal()); // 0-成功
-                        restTemplate.postForEntity("http://reminder-service/api/listResultMessageResource", listResult, Void.class);
-                        progressMessage.setCurrent(5);
-                        rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
+                } finally {
+                    // 关闭流
+                    if (workbook != null) {
+                        try {
+                            workbook.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (out != null) {
+                        try {
+                            out.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fileOutputStream != null) {
+                        try {
+                            fileOutputStream.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    // 删除文件
+                    if (file != null) {
+                        file.delete();
                     }
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-                List<String> urls = new ArrayList<>();
-                ListResult listResult = new ListResult();
-                urls.add("导出失败！");
-                listResult.setStatus(ListResult.Status.FAILURE.getVal()); // 1-失败
-                restTemplate.postForEntity("http://reminder-service/api/listResultMessageResource", listResult, Void.class);
-                progressMessage.setCurrent(5);
-                rabbitTemplate.convertAndSend(Constants.FOLLOWUP_EXPORT_QE, progressMessage);
-            } finally {
-                // 关闭流
-                if (workbook != null) {
-                    try {
-                        workbook.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (out != null) {
-                    try {
-                        out.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (fileOutputStream != null) {
-                    try {
-                        fileOutputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                // 删除文件
-                if (file != null) {
-                    file.delete();
-                }
-            }
-            return ResponseEntity.ok().headers(HeaderUtil.createAlert("开始导出,完成后请前往消息列表查看下载。", "")).body(url);
+            });
+            t.start();
+            return ResponseEntity.ok().headers(HeaderUtil.createAlert("开始导出,完成后请前往消息列表查看下载。", "")).body(null);
         } catch (Exception ex) {
             log.error(ex.getMessage());
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "", "上传文件服务器失败")).body(null);
         }
     }
+
 }
