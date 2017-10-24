cde52a2f90ec50d1c69ebce61a416c8b344f7f8c
hukaijia <hukaijia>
2017-10-24 18:22:54 +0800

    添加系统注册的功能

--- service/business/src/main/java/cn/fintecher/pangolin/business/scheduled/SystemBackupScheduled.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/scheduled/SystemBackupScheduled.java
@@ -1,13 +1,13 @@
 package cn.fintecher.pangolin.business.scheduled;
 
+import cn.fintecher.pangolin.business.repository.CompanyRepository;
 import cn.fintecher.pangolin.business.repository.SysParamRepository;
 import cn.fintecher.pangolin.business.repository.SystemBackupRepository;
 import cn.fintecher.pangolin.business.service.SystemBackupService;
-import cn.fintecher.pangolin.entity.QSysParam;
-import cn.fintecher.pangolin.entity.SysParam;
-import cn.fintecher.pangolin.entity.SystemBackup;
+import cn.fintecher.pangolin.entity.*;
 import cn.fintecher.pangolin.entity.util.Constants;
 import cn.fintecher.pangolin.util.ZWDateUtil;
+import org.apache.commons.collections4.IteratorUtils;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -39,6 +40,8 @@ public class SystemBackupScheduled {
     private SystemBackupService systemBackupService;
     @Autowired
     private SysParamRepository sysParamRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
 
     @Scheduled(cron = "0 0 */23 * * ?")
     void systemBackup() throws IOException {
@@ -56,8 +59,8 @@ public class SystemBackupScheduled {
                     request.setMysqlName(result);
 //mongodb数据库备份
                     Iterator<SysParam> mongodbSysParams = sysParamRepository.findAll(qSysParam.code.eq(Constants.MONGODB_BACKUP_ADDRESS_CODE).and(qSysParam.type.eq(Constants.MONGODB_BACKUP_ADDRESS_TYPE))).iterator();
-                    if(mongodbSysParams.hasNext()) {
-                        String mongodbResult = systemBackupService.operationShell(mongodbSysParams.next().getValue(),"Administrator");
+                    if (mongodbSysParams.hasNext()) {
+                        String mongodbResult = systemBackupService.operationShell(mongodbSysParams.next().getValue(), "Administrator");
                         Pattern mongodbp = Pattern.compile(".*mongodb");
                         Matcher mongodbm = mongodbp.matcher(mongodbResult);
                         while (mongodbm.find()) {
@@ -71,6 +74,22 @@ public class SystemBackupScheduled {
                 }
             }
 
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Scheduled(cron = "0 */23 */23 * * ?")
+    void reduceSoftwareDay() throws IOException {
+        log.info("开始注册天数跑批" + new DateTime().toString("yyyy-MM-dd HH:mm:ss"));
+        try {
+            QCompany qCompany = QCompany.company;
+            Iterator<Company> company = companyRepository.findAll(qCompany.registerDay.isNotNull().and(qCompany.registerDay.ne(0).and(qCompany.registerDay.ne(99999)))).iterator();
+            List<Company> companyList = IteratorUtils.toList(company);
+            for (Company company1 : companyList) {
+                company1.setRegisterDay(company1.getRegisterDay() - 1);
+                companyRepository.save(company1);
+            }
         } catch (Exception e1) {
             e1.printStackTrace();
         }
