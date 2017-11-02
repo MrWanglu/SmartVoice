0d810073873e1136dfa94db1d52826d1a2238dc9
xiaqun <qun.xia@zzlgroup.com>
2017-11-02 16:18:30 +0800

    优化了电催，外访主界面的多条件搜索

--- service/report/src/main/java/cn/fintecher/pangolin/report/mapper/CaseInfoMapper.java
+++ service/report/src/main/java/cn/fintecher/pangolin/report/mapper/CaseInfoMapper.java
@@ -2,11 +2,14 @@ package cn.fintecher.pangolin.report.mapper;
 
 import cn.fintecher.pangolin.entity.Personal;
 import cn.fintecher.pangolin.report.entity.CaseInfo;
+import cn.fintecher.pangolin.report.model.CaseInfoModel;
 import cn.fintecher.pangolin.report.model.CaseInfoParams;
 import cn.fintecher.pangolin.report.model.CollectingCaseInfo;
 import cn.fintecher.pangolin.report.model.CollectingCaseParams;
 import cn.fintecher.pangolin.report.util.MyMapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -15,7 +18,7 @@ import java.util.List;
  * @Date 17:50 2017/8/1
  */
 public interface CaseInfoMapper extends MyMapper<CaseInfo> {
-//    List<CaseInfo> queryWaitCollectCase(@Param("deptCode") String deptCode,
+    //    List<CaseInfo> queryWaitCollectCase(@Param("deptCode") String deptCode,
 //                                        @Param("companyCode") String companyCode);
     List<CaseInfo> queryWaitCollectCase(CaseInfoParams caseInfoParams);
 
@@ -24,5 +27,30 @@ public interface CaseInfoMapper extends MyMapper<CaseInfo> {
     List<CollectingCaseInfo> queryCollectingCase(CollectingCaseParams collectingCaseParams);
 
     void updateLngLat(Personal personal);
+
+    /**
+     * @Description 多条件查询案件信息
+     */
+    List<CaseInfoModel> getCaseInfoByCondition(
+            @Param("personalName") String personalName,
+            @Param("mobileNo") String mobileNo,
+            @Param("deptCode") String deptCode,
+            @Param("collectorName") String collectorName,
+            @Param("overdueMaxAmt") BigDecimal overdueMaxAmt,
+            @Param("overdueMinAmt") BigDecimal overdueMinAmt,
+            @Param("payStatus") String payStatus,
+            @Param("overMaxDay") Integer overMaxDay,
+            @Param("overMinDay") Integer overMinDay,
+            @Param("batchNumber") String batchNumber,
+            @Param("principalId") String principalId,
+            @Param("idCard") String idCard,
+            @Param("feedBack") Integer feedBack,
+            @Param("assistWay") Integer assistWay,
+            @Param("caseMark") Integer caseMark,
+            @Param("collectionType") Integer collectionType,
+            @Param("sort") String sort,
+            @Param("code") String code,
+            @Param("collectionStatusList") String collectionStatusList,
+            @Param("collectionStatus") Integer collectionStatus);
 
 }
---
+++ service/report/src/main/java/cn/fintecher/pangolin/report/model/CaseInfoModel.java
@@ -0,0 +1,41 @@
+package cn.fintecher.pangolin.report.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author : xiaqun
+ * @Description : 展示用案件模型
+ * @Date : 10:19 2017/11/1
+ */
+
+@Data
+public class CaseInfoModel {
+    private String caseId; //案件ID
+    private String caseNumber; //案件编号
+    private String batchNumber; //批次号
+    private String personalName; //客户姓名
+    private String mobileNo; //手机号
+    private String idCard; //身份证号
+    private String overdueAmount; //案件金额
+    private String payStatus; //还款状态
+    private Integer overdueDays; //逾期天数
+    private String principalName; //委托方名称
+    private String collectorName; //催收员姓名
+    private Integer collectionStatus; //催收状态
+    private Date follTime; //跟进时间
+    private Date caseFollowInTime; //案件流入日期
+    private Integer followupBack; //催收反馈
+    private Integer assistWay; //协催方式
+    private Integer holdDays; //持案天数
+    private Integer leftDays; //剩余天数
+    private Integer leaveCaseFlag; //留案标识
+    private String deptCode; //部门code码
+    private Integer collectionType; //催收类型
+    private Integer caseMark; //案件标记
+    private String personalId; //客户信息ID
+    private BigDecimal promiseAmt; //承诺还款金额
+    private Date promiseTime; //承诺还款日期
+}
