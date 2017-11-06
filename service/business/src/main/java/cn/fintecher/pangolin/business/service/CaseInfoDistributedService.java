8d50ebbd143852039578bb19cfee136aefeefc39
sunyanping <sunyanping24@sina.com>
2017-11-06 18:47:18 +0800

    优化代码

--- service/business/src/main/java/cn/fintecher/pangolin/business/service/CaseInfoDistributedService.java
+++ service/business/src/main/java/cn/fintecher/pangolin/business/service/CaseInfoDistributedService.java
@@ -12,7 +12,6 @@ import org.kie.api.runtime.KieSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
