54c84552e7b98a3f96198876074c431a6de6ae08
james <457237252@qq.com>
2017-11-10 11:49:39 +0800

    修改代码

--- service/dataimp/src/main/java/cn/fintecher/pangolin/dataimp/scheduled/UserVideoScheduled.java
+++ service/dataimp/src/main/java/cn/fintecher/pangolin/dataimp/scheduled/UserVideoScheduled.java
@@ -66,12 +66,16 @@ public class UserVideoScheduled {
                             if (Objects.nonNull(fileNewList) && fileNewList.length > 0) {
                                 for (int j = 0; j < fileNewList.length; j++) {
                                     if (fileNewList[j].toString().endsWith(".WAV") || fileNewList[j].toString().endsWith(".wav")) {
+                                        //转化成mp3
                                         String newPath = VideoSwitchUtil.switchToMp3(filePath.concat(ZWDateUtil.getFormatNowDate("yyyyMMdd")).concat("\\").concat(filelist[i].toString()).concat("\\".concat(fileNewList[j].toString())), ffmpeg);
+                                        //获取时长
                                         String videoLength = VideoSwitchUtil.getVideoTime(filePath.concat(ZWDateUtil.getFormatNowDate("yyyyMMdd")).concat("\\").concat(filelist[i].toString()).concat("\\".concat(fileNewList[j].toString())), ffmpeg);
+                                        //上传文件服务器
                                         MultiValueMap<String, Object> param = new LinkedMultiValueMap<>();
                                         FileSystemResource fileSystemResource = new FileSystemResource(newPath);
                                         param.add("file", fileSystemResource);
                                         String url = restTemplate.postForObject("http://file-service/api/uploadFile/addUploadFileUrl", param, String.class);
+                                        //新增记录
                                         if (!ZWStringUtils.isEmpty(url)) {
                                             UserVideo userVideo = new UserVideo();
                                             userVideo.setCompanyCode("0001");
@@ -88,9 +92,11 @@ public class UserVideoScheduled {
                                     }
                                 }
                             }
+                            logger.error("该催收员下面没有找到对应的录音文件");
                         }
                     }
                 }
+                logger.error("日期下面没有找到催收员对应的文件夹");
             }
             //创建新文件 下一天用
             Calendar calendar = Calendar.getInstance();
