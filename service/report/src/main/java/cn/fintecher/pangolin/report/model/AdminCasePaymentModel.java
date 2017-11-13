9e4820d479d915ce2267bbfacac5e3d012639552
james <457237252@qq.com>
2017-11-13 17:10:31 +0800

    修改代码

--- service/report/src/main/java/cn/fintecher/pangolin/report/model/AdminCasePaymentModel.java
+++ service/report/src/main/java/cn/fintecher/pangolin/report/model/AdminCasePaymentModel.java
@@ -13,15 +13,19 @@ import java.math.BigDecimal;
 public class AdminCasePaymentModel {
 
     @ApiModelProperty("每年/月/周案件金额")
-    private BigDecimal amount = new BigDecimal(0);
+    private BigDecimal caseAmount = new BigDecimal(0);
     @ApiModelProperty("案件总金额(万元)")
-    private BigDecimal totalAmount = new BigDecimal(0);
+    private BigDecimal totalCaseAmount = new BigDecimal(0);
     @ApiModelProperty("每年/月/周案件数量")
     private Integer caesCount = 0;
     @ApiModelProperty("案件总数量")
     private Integer totalCaseCount = 0;
     @ApiModelProperty("查询类型 0 全部 1 内催 2 委外")
     private Integer queryType;
+    @ApiModelProperty("月")
+    private String queryMonth;
+    @ApiModelProperty("日")
+    private String queryDate;
     @ApiModelProperty("还款类型 0 已还款 1 还款审核中")
     private Integer collectionStatus;
 }
