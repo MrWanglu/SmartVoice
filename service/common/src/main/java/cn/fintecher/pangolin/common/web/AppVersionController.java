e5e406ea8b3b35bfe6a90eec60ceea69711fb507
qijigui <457237252@qq.com>
2017-11-23 16:21:48 +0800

    Merge branch '2.2.0'
    # Conflicts:
    #	service/business/src/main/java/cn/fintecher/pangolin/business/web/CaseInfoController.java
    #	service/report/src/main/java/cn/fintecher/pangolin/report/mapper/CaseInfoMapper.java
    #	service/report/src/main/java/cn/fintecher/pangolin/report/model/PromisedModel.java
    #	service/report/src/main/java/cn/fintecher/pangolin/report/service/HomePageService.java
    #	service/report/src/main/java/cn/fintecher/pangolin/report/web/CaseInfoInquiryController.java
    #	service/report/src/main/resources/mapper/AdminPageMapper.xml
    #	service/report/src/main/resources/mapper/CaseInfoMapper.xml

--- service/common/src/main/java/cn/fintecher/pangolin/common/web/AppVersionController.java
+++ service/common/src/main/java/cn/fintecher/pangolin/common/web/AppVersionController.java
@@ -9,6 +9,7 @@ import cn.fintecher.pangolin.entity.User;
 import cn.fintecher.pangolin.entity.util.Constants;
 import cn.fintecher.pangolin.entity.util.EntityUtil;
 import cn.fintecher.pangolin.util.ZWDateUtil;
+import cn.fintecher.pangolin.util.ZWStringUtils;
 import cn.fintecher.pangolin.web.HeaderUtil;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
@@ -26,7 +27,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.annotations.ApiIgnore;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
@@ -54,17 +57,13 @@ public class AppVersionController {
                                                              @RequestHeader(value = "X-UserToken") String token) {
         condition = (AppVersionSaveCondition) EntityUtil.emptyValueToNull(condition);
         User user = userClient.getUserByToken(token).getBody();
-        if (!(Objects.equals(user.getId(), Constants.ADMINISTRATOR_ID))) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME,
-                    "Can't add without permission", "没有权限不能添加")).body(null);
-        }
         String regex = "^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$";
-        if (!condition.getAppVersion().matches(regex)) {
+        if (!condition.getMobileVersion().matches(regex)) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "The version number is not in conformity with the rules", "版本号不符合规则")).body(null);
         }
         for (String os : condition.getOs()) {
-            QAppVersion qAppVersion = QAppVersion.appVersion1;
-            Iterable<AppVersion> appVersions = appVersionRepository.findAll(qAppVersion.appVersion.eq(condition.getAppVersion()).and(qAppVersion.os.eq(os)));
+            QAppVersion qAppVersion = QAppVersion.appVersion;
+            Iterable<AppVersion> appVersions = appVersionRepository.findAll(qAppVersion.mobileVersion.eq(condition.getMobileVersion()).and(qAppVersion.os.eq(os)));
             if (appVersions.iterator().hasNext()) {
                 return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "This version has been in existence", "该版本已存在")).body(null);
             }
@@ -77,6 +76,7 @@ public class AppVersionController {
             appVersion.setCreator(user.getUserName());
             appVersion.setCreateTime(ZWDateUtil.getNowDateTime());
             appVersion.setPublishTime(ZWDateUtil.getNowDateTime());
+            appVersion.setCompanyCode(user.getCompanyCode());
             AppVersion appVersion1 = appVersionRepository.save(appVersion);
             appVersionList.add(appVersion1);
         }
@@ -85,47 +85,60 @@ public class AppVersionController {
 
     @GetMapping(value = "/deleteAppVersion")
     @ApiOperation(value = "删除app版本", notes = "删除app版本")
-    public ResponseEntity<Void> deleteAppVersion(@RequestParam String id,
-                                                 @RequestHeader(value = "X-UserToken") String token) {
-        User user = userClient.getUserByToken(token).getBody();
-        if (!(Objects.equals(user.getId(), Constants.ADMINISTRATOR_ID))) {
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME,
-                    "Can't add without permission", "没有权限不能添加")).body(null);
+    public ResponseEntity<Void> deleteAppVersion(@RequestParam String id) {
+        try {
+            appVersionRepository.delete(id);
+            return ResponseEntity.ok().body(null);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("", "", "删除失败")).body(null);
         }
-        appVersionRepository.delete(id);
-        return ResponseEntity.ok().body(null);
     }
 
     @GetMapping("/queryAppVersion")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "page", dataType = "int", paramType = "query",
-                    value = "页数 (0..N)"),
-            @ApiImplicitParam(name = "size", dataType = "int", paramType = "query",
-                    value = "每页大小."),
-            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string", paramType = "query",
-                    value = "依据什么排序: 属性名(,asc|desc). ")
-    })
     @ApiOperation(value = "分页查询app版本控制", notes = "分页查询app版本控制")
-    public ResponseEntity<Page<AppVersion>> queryAppVersion(@QuerydslPredicate(root = AppVersion.class) Predicate predicate,
-                                                            @ApiIgnore Pageable pageable,
+    public ResponseEntity<Page<AppVersion>> queryAppVersion(Pageable pageable,
                                                             @RequestHeader(value = "X-UserToken") String token,
-                                                            @RequestParam(value = "companyCode", required = false) @ApiParam("公司Code码") String companyCode) {
+                                                            @RequestParam(value = "companyCode", required = false) @ApiParam("公司Code码") String companyCode,
+                                                            @RequestParam(value = "os", required = false) String os,
+                                                            @RequestParam(value = "pubDateBegin", required = false) String pubDateBegin,
+                                                            @RequestParam(value = "pubDateEnd", required = false) String pubDateEnd,
+                                                            @RequestParam(value = "mobileVersion", required = false) String mobileVersion) {
 
         User user;
-        BooleanBuilder builder = new BooleanBuilder(predicate);
         try {
-            ResponseEntity<User> userEntity = restTemplate.getForEntity(Constants.USERTOKEN_SERVICE_URL.concat(token), User.class);
-            user = userEntity.getBody();
-            if (Objects.nonNull(user.getCompanyCode())) {
-                builder.and(QAppVersion.appVersion1.companyCode.eq(user.getCompanyCode()));
+
+
+            BooleanBuilder builder = new BooleanBuilder();
+            try {
+                ResponseEntity<User> userEntity = restTemplate.getForEntity(Constants.USERTOKEN_SERVICE_URL.concat(token), User.class);
+                user = userEntity.getBody();
+                if (Objects.nonNull(user.getCompanyCode())) {
+                    builder.and(QAppVersion.appVersion.companyCode.eq(user.getCompanyCode()));
+                }
+                logger.debug("REST request to query company : {}");
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+                return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "AppVersion", "系统异常!")).body(null);
             }
-            logger.debug("REST request to query company : {}");
-        } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "AppVersion", "系统异常!")).body(null);
+            if (ZWStringUtils.isNotEmpty(os)) {
+                builder.and(QAppVersion.appVersion.os.eq(os));
+            }
+            if (ZWStringUtils.isNotEmpty(mobileVersion)) {
+                builder.and(QAppVersion.appVersion.mobileVersion.eq(mobileVersion));
+            }
+            if (ZWStringUtils.isNotEmpty(pubDateBegin)) {
+                String beginDate = pubDateBegin + " 00:00:00";
+                String endDate = pubDateEnd + " 23:59:59";
+                builder.and(QAppVersion.appVersion.publishTime.goe(ZWDateUtil.getUtilDate(beginDate, "yyyy-MM-dd HH:mm:ss")));
+                builder.and(QAppVersion.appVersion.publishTime.loe(ZWDateUtil.getUtilDate(endDate, "yyyy-MM-dd HH:mm:ss")));
+            }
+            Page<AppVersion> page = appVersionRepository.findAll(builder, pageable);
+            return ResponseEntity.ok().headers(HeaderUtil.createAlert("查询成功", ENTITY_NAME)).body(page);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("", "", "查询失败")).body(null);
         }
-        Page<AppVersion> page = appVersionRepository.findAll(builder, pageable);
-        return ResponseEntity.ok().headers(HeaderUtil.createAlert("查询成功", ENTITY_NAME)).body(page);
     }
 
 
@@ -136,10 +149,10 @@ public class AppVersionController {
         if (!version.matches(regex)) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert(ENTITY_NAME, "The version number abnormality", "版本号异常")).body(null);
         }
-        QAppVersion qAppVersion = QAppVersion.appVersion1;
+        QAppVersion qAppVersion = QAppVersion.appVersion;
         Iterable<AppVersion> appVersions = appVersionRepository.findAll(qAppVersion.os.eq(os), new Sort(Sort.Direction.DESC, "publishTime"));
         if (appVersions.iterator().hasNext()) {
-            if (Objects.equals(appVersions.iterator().next().getAppVersion(), version)) {
+            if (Objects.equals(appVersions.iterator().next().getMobileVersion(), version)) {
                 return ResponseEntity.ok().body(null);
             } else {
                 return ResponseEntity.ok().body(appVersions.iterator().next());
